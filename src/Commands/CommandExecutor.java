13b1523d5d5fb4a15ec225534a83bbe2262f3cf5
kirillvelyugo <kirillvelyugo97@gmail.com>
2023-04-27 08:08:57 +0300

    feat: add FilterLessThanUnitOfMeasure command

--- src/Commands/CommandExecutor.java
+++ src/Commands/CommandExecutor.java
@@ -26,6 +26,7 @@ public class CommandExecutor {
         this.commands.put("remove_greater_key", new RemoveGreaterKey(this.collectionManager));
         this.commands.put("remove_lover_key", new RemoveLoverKey(this.collectionManager));
         this.commands.put("max_by_name", new MaxByName(this.collectionManager));
+        this.commands.put("filter_less_than_unit_of_measure", new FilterLessThanUnitOfMeasure(this.collectionManager));
 
     }
 
---
+++ src/Commands/FilterLessThanUnitOfMeasure.java
@@ -0,0 +1,43 @@
+package Commands;
+
+import Collection.UnitOfMeasure;
+import CollectionManager.CollectionManager;
+import Expections.WrongArguments;
+
+import java.util.Set;
+
+public class FilterLessThanUnitOfMeasure implements Command{
+
+    private final CollectionManager collectionManager;
+
+    public FilterLessThanUnitOfMeasure(CollectionManager collectionManager) {
+        this.collectionManager = collectionManager;
+    }
+
+    @Override
+    public void execute(String[] args) throws WrongArguments {
+        if (args.length < 2) throw new WrongArguments("Not enough arguments");
+        UnitOfMeasure bound;
+        try {
+            bound = UnitOfMeasure.valueOf(args[1].toUpperCase());
+        }
+        catch (IllegalArgumentException e){
+            throw new WrongArguments("No such unit of measure");
+        }
+
+        Set<String> keyset = collectionManager.getKeySet();
+        for(String key : keyset){
+            if (collectionManager.getByKey(key).getUnitOfMeasure() != null){
+                if (collectionManager.getByKey(key).getUnitOfMeasure().compareTo(bound) < 0) {
+                    System.out.println(collectionManager.getByKey(key));
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public String info() {
+        return "output elements whose unit Of Measure field value is less than the specified one";
+    }
+}
